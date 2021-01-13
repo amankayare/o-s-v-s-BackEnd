dcacb6efece09420f58962e136d67352a1732399
amankayare <amankayare@gmail.com>
2021-01-13 17:10:51 +0530

    initial commit

---
+++ src/main/java/com/cdac/osvs/OsvsApplication.java
@@ -0,0 +1,13 @@
+package com.cdac.osvs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsvsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsvsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdac/osvs/OsvsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdac.osvs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsvsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
