9632115f8fbfc7139866115aca583f1c46ee3e90
amankayare <amankayare@gmail.com>
2021-01-13 17:22:34 +0530

    data source configuration added

--- src/main/java/com/cdac/osvs/OsvsApplication.java
+++ src/main/java/com/cdac/osvs/OsvsApplication.java
@@ -2,12 +2,19 @@ package com.cdac.osvs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class OsvsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OsvsApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String sayHello(){
+		return "Hey Hello";
+	}
 }
